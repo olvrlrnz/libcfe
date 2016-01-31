2767a10836781aadee087abae9776e67ebb72c79
Oliver Lorenz <olli@olorenz.org>
2016-01-31 13:42:09 +0100

    Introduce CFE_FILEWRITER_VERSION_DEFAULT define Signed-off-by: Oliver Lorenz <olli@olorenz.org>
    CFE_FILEWRITER_VERSION_DEFAULT can be used to allocate a file writer of the
    writer type currently set as default writer.

--- include/fs/filewriter.h
+++ include/fs/filewriter.h
@@ -2,15 +2,24 @@
 #define LIBCFE_FS_FILEWRITER_H
 
 
+#include <inttypes.h>
 #include <cfe/list.h>
 #include <cfe/atomic.h>
 
 
+#define CFE_FILEWRITER_VERSION_DEFAULT		(-1)
+
+
 struct cfe_filewriter_type;
+struct cfe_cipher_type;
 
 
 struct cfe_filewriter {
+	uint32_t blocksize;
 	pthread_rwlock_t lock;
+	unsigned char *key;
+	size_t keysize;
+	struct cfe_cipher_type *cipher;
 	struct cfe_filewriter_type *type;
 	const struct cfe_filewriter_ops *ops;
 };
@@ -22,7 +31,8 @@ struct cfe_filewriter_ops {
 struct cfe_filewriter_type {
 	unsigned version;
 	cfe_atomic_t refcnt;
-	struct cfe_filewriter *(*alloc)(void);
+	struct cfe_filewriter *(*alloc)(struct cfe_cipher_type *cipher,
+	                                uint32_t blocksize);
 	struct list_head next;
 };
 
@@ -30,6 +40,8 @@ struct cfe_filewriter_type {
 extern int cfe_filewriter_register(struct cfe_filewriter_type *type);
 extern int cfe_filewriter_unregister(struct cfe_filewriter_type *type);
 
-extern struct cfe_filewriter *cfe_filewriter_create(int version);
+extern struct cfe_filewriter *cfe_filewriter_create(int version,
+                                                    const char *cipher,
+                                                    uint32_t blocksize);
 
 #endif
